f91945ce6214b698d2f6229aebd80cabfbbc5951
Nicolai <niiicolai@gmail.com>
2026-02-19 21:11:23 +0100

    add new cert

--- src/views/RootView.vue
+++ src/views/RootView.vue
@@ -57,6 +57,7 @@ onMounted(() => {
             <CredlyBadge badge-id="e74f1a47-2fa9-4e90-8fe9-7b57686b281c" width="250" height="250" />
             <CredlyBadge badge-id="7a71ead5-0c1f-41a2-99be-f17d8ed4f27d" width="250" height="250" />
             <CredlyBadge badge-id="c7e5a093-683a-4182-b1af-6079753f0336" width="250" height="250" />
+            <CredlyBadge badge-id="12eebda4-0579-4ac8-97d0-75e4323158d7" width="250" height="250" />
           </div>
         </div>
 
@@ -109,8 +110,8 @@ onMounted(() => {
         </div>
 
         <div class="bg-neutral-900 border border-neutral-500 rounded-md text-neutral-200 overflow-hidden">
-          <img src="/examples/ai-example-page.png"
-              alt="AI website example" class="w-full border-b border-neutral-500" />
+          <img src="/examples/ai-example-page.png" alt="AI website example"
+            class="w-full border-b border-neutral-500" />
 
           <div class="p-3 flex flex-col gap-2">
             <a href="https://bergandersen.com/ai" target="_blank" class="text-xl block font-extrabold hover:underline">
