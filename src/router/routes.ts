b61e60f5b8ae081afd5517172ebc2d3bb39acc5a
Nicolai <niiicolai@gmail.com>
2026-03-31 07:42:08 +0200

    remove content

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,10 +2,12 @@ import { createWebHashHistory, createRouter } from 'vue-router'
 
 import RootView from '../views/RootView.vue'
 import SceneView from '../views/SceneView.vue'
+import LastView from '../views/LastView.vue'
 import NotFoundView from '../views/error/NotFound.vue'
 
 const routes = [
-  { path: '/', component: RootView },
+  { path: '/', component: LastView },
+  { path: '/root', component: RootView },
   { path: '/os', component: SceneView },
   { path: '/:pathMatch(.*)*', component: NotFoundView },
 ]
---
+++ src/views/LastView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="last-view">
+    No content.
+  </div>
+</template>
+
+<style scoped>
+.last-view {
+  background: black;
+  color: #fff;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
