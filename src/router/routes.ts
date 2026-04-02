4c760926936c57b089bb20b6c30350cd3a130d84
Nicolai <niiicolai@gmail.com>
2026-04-02 10:38:42 +0200

    update

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,12 +2,10 @@ import { createWebHashHistory, createRouter } from 'vue-router'
 
 import RootView from '../views/RootView.vue'
 import SceneView from '../views/SceneView.vue'
-import LastView from '../views/LastView.vue'
 import NotFoundView from '../views/error/NotFound.vue'
 
 const routes = [
-  { path: '/', component: LastView },
-  { path: '/root', component: RootView },
+  { path: '/', component: RootView },
   { path: '/os', component: SceneView },
   { path: '/:pathMatch(.*)*', component: NotFoundView },
 ]
